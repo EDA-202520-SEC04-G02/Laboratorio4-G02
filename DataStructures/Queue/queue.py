4252c0737669d3077b02aabb5f7db71d4c125f56
LuigiToby <la.rodrigueza12@uniandes.edu.co>
2025-08-28 12:13:21 -0500

    queue con single_linked_list

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,17 +1,22 @@
 from DataStructures.List import single_linked_list as sl
 
 def new_queue():
-    """
-    Crea una nueva cola
-    """
-    queue = sl.new_list()
+    return sl.new_list()
 
-    return queue
+def is_empty(queue):
+    return sl.is_empty(queue)
+
+def size(queue):
+    return sl.size(queue)
 
 def enqueue(queue, element):
-    """
-    Agrega un elemento a la cola
-    """
-    sl.add_last(queue['elements'], element)
-    queue['size'] += 1
+    queue = sl.add_last(queue, element)
     return queue
+
+def dequeue(queue):
+    queue = sl.remove_first(queue)
+    return queue
+
+def peek(queue):
+    return sl.get_element(queue, 0)
+
