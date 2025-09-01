b2af506ecf5d9c92abdf29f1dc613ef51c104fb3
LuigiToby <la.rodrigueza12@uniandes.edu.co>
2025-08-31 21:35:01 -0500

    print era top y no peek, confundí el nombre

--- App/view.py
+++ App/view.py
@@ -93,7 +93,7 @@ def print_tests_results(queue_results, stack_results):
     print("Tiempo de ejecución para push:",
           f"{stack_results['push_time']:.3f}", "[ms]")
     print("Tiempo de ejecución para peek:",
-          f"{stack_results['peek_time']:.3f}", "[ms]")
+          f"{stack_results['top_time']:.3f}", "[ms]")
     print("Tiempo de ejecución para pop:",
           f"{stack_results['pop_time']:.3f}", "[ms]")
 
